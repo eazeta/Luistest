8394312b2f4f237e0bfd6388a2dc2f632cebaab0
eazeta <emmanuel.2000z@gmail.com>
2022-12-31 18:01:03 +0000

    test

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ function App() {
 	return (
 		<div className='App'>
 			<h1>Hi Luis</h1>
+<<<<<<< HEAD
 			<h2>Hi Emmanuel</h2>
 			<h3>Aaron</h3>
 			<h4>Wikipedia</h4>
 			<h5>Google Images</h5>
+=======
+>>>>>>> parent of 92fd956... second addition
 		</div>
 	);
 }
